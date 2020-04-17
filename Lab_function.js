187896c5b1f937c393f6d9f0cba7d633f921d7c2
AleksandrBo850 <54817599+AleksandrBo850@users.noreply.github.com>
2020-04-17 17:13:24 +0300

    Add files via upload

---
+++ Lab_function.js
@@ -0,0 +1,19 @@
+let arg = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+//        function declaration
+function amount(x, y, z = 10) {
+    return x + y * z;
+}; //фу-ция x+y*10
+//        function expression
+let arrayFunc = function (x) {
+
+    for (i = 0; i <= arg.length; i++) { 
+        if (x == arg[i]) {
+            console.log(`Значение ${x} входит в данный массив`);
+        } else {
+            console.log(arg[i]);
+
+        }
+    }
+};
+console.log(amount(10, 5));
+arrayFunc(7);
